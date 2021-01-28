86f95bf6368508a469aa482301b7385636cb737c
katemezha <44982634+katemezha@users.noreply.github.com>
2021-01-28 13:52:28 +0300

    bbb

--- admin.php
+++ admin.php
@@ -8,11 +8,11 @@
 		// if(isset($_POST['new'])){
 		// 	header('Location:new.php')
 		// }
-		if (isset($_POST['vopros1'])) {
-			echo '<form method="POST"><p>'.$_POST['text1'].'<input type="" name="otv1"></p><input type="submit" name="new" value="Отправить"></form>';
-		}
-		if (isset($_POST['vopros2'])) {
-			echo '<form method="POST"><p>'.$_POST['text2'].'<input type="textarea" name="otv2"></p><input type="submit" name="new" value="Отправить"></form>';
+		if (isset($_POST['new'])) {
+			echo '<form method="POST"><p>'.$_POST['text1'].'<input type="" name="otv1" attern="^[ 0-9]+$"//></p><input type="submit" name="new" value="Отправить"></form>';
+			echo '<form method="POST"><p>'.$_POST['text2'].'<input type="" name="otv2"></p><input type="submit" name="new" value="Отправить"></form>';
+			echo '<form method="POST"><p>'.$_POST['text3'].'<input type="" name="otv3"></p><input type="submit" name="new" value="Отправить"></form>';
+			echo '<form method="POST"><p>'.$_POST['text4'].'<input type="textarea" name="otv4"></p><input type="submit" name="new" value="Отправить"></form>';
 		}
 
 	?>
@@ -20,7 +20,16 @@
 	<form method="POST">
 		<p>Тип 1:</p>
 		<input type="" name="text1">
-		<input type="submit" name="vopros1" value="Добавить текстовое поле">
+		<input type="submit" name="vopros1" value="Ответ число">
+		<p>Тип 2:</p>
+		<input type="" name="text2">
+		<input type="submit" name="vopros2" value="Ответ положительное число">
+		<p>Тип 3:</p>
+		<input type="" name="text3">
+		<input type="submit" name="vopros3" value="Добавить текстовое поле">
+		<p>Тип 4:</p>
+		<input type="" name="text4">
+		<input type="submit" name="vopros4" value="Добавить многострочное поле">
 		<p>Тип 2:</p>
 		<input type="" name="text2">
 		<input type="submit" name="vopros2" value="Добавить многострочное поле">
